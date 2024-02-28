927ca358e47906013949d38eaeb2e7018c3cc446
wxc <softsora@163.com>
2024-02-28 18:51:00 +0800

    beer

--- src/main.c
+++ src/main.c
@@ -1,22 +1,19 @@
 #include <stm32f10x.h>
 #include <delay.h>
 int main(void){
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     GPIO_InitTypeDef init;
     init.GPIO_Mode = GPIO_Mode_Out_PP;
-    uint16_t pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+    uint16_t pin = GPIO_Pin_12;
     init.GPIO_Pin = pin;
     init.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &init);
+    GPIO_Init(GPIOB, &init);
 
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
     while(1)
     {
-        GPIO_Write(GPIOA, ~0x0001);
+        GPIO_WriteBit(GPIOB, GPIO_Pin_12, Bit_RESET);
         Delay_s(1);
-        GPIO_Write(GPIOA, ~0x0002);
-        Delay_s(1);
-        GPIO_Write(GPIOA, ~0x0004);
+        GPIO_WriteBit(GPIOB, GPIO_Pin_12, Bit_SET);
         Delay_s(1);
     }
 }
