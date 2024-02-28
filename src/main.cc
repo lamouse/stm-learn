a10b209539611b0654f7e9e6e7d45b7c0693ee27
wxc <softsora@163.com>
2024-02-28 17:53:06 +0800

    delay

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <stm32f10x.h>
+#include <Delay.h>
 auto main()->int{
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef init;
@@ -11,6 +12,7 @@ auto main()->int{
     while(true)
     {
         GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+        Delay_s(1);
         GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
     }
 }
