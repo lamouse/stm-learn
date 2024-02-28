4319f8a4cae223192248303c4eaf1a9262c7a445
wxc <softsora@163.com>
2024-02-28 18:38:56 +0800

    led2

--- src/main.c
+++ src/main.c
@@ -4,16 +4,19 @@ int main(void){
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef init;
     init.GPIO_Mode = GPIO_Mode_Out_PP;
-    init.GPIO_Pin = GPIO_Pin_0;
+    uint16_t pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+    init.GPIO_Pin = pin;
     init.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &init);
 
     GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
     while(1)
     {
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+        GPIO_Write(GPIOA, ~0x0001);
         Delay_s(1);
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+        GPIO_Write(GPIOA, ~0x0002);
+        Delay_s(1);
+        GPIO_Write(GPIOA, ~0x0004);
         Delay_s(1);
     }
 }
