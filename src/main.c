22e47eff1c7789fc0edee9652a6aae0bacca5b5a
wxc <softsora@163.com>
2024-02-28 21:57:23 +0800

    LED

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <stm32f10x.h>
 #include <delay.h>
+#include <OLED.h>
 int main(void){
-
+    OLED_Init();
+    OLED_ShowChar(1, 1, 'A');
+    OLED_ShowString(1, 3, "HELLO word");
+    OLED_ShowSignedNum(2,1, -12, 2);
+    OLED_ShowBinNum(3,1, 0xaaa5, 16);
+    while (1)
+    {
+    }
 }
