432c842d22446467686c1c0f062ac25d85e4f097
wxc <softsora@163.com>
2024-03-01 17:06:33 +0800

    TIM 编码器测速

--- src/hardware/include/encoder.h
+++ src/hardware/include/encoder.h
@@ -1,6 +1,8 @@
 #ifndef ENCODER_H_
 #define ENCODER_H_
 #include <stm32f10x.h>
-void Encoder_init();
-int16_t getEncodeCount();
+void Encoder_init(void);
+int16_t getEncodeCount(void);
+void TIM_Ecoder_init(void);
+int16_t TIM_Encoder_get(void);
 #endif
